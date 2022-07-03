cfcaaeb933212b6cf3c85bfd9f41ea5a8f4df578
rovin-singh <91409265+rovin-singh@users.noreply.github.com>
2022-07-03 18:28:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Forms from "./components/Forms";
 import "./styles.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Forms />
+    </div>
+  );
 }
 
 export default App;
